29f294cffb4e63d38099daf3a445664074327932
pth1004 <theajsky@gmail.com>
2022-12-09 16:41:49 +0900

    profile , device 초안 주석 commit

---
+++ models/device.js
@@ -0,0 +1,53 @@
+/* eslint-disable max-len */
+// const Sequelize = require('sequelize');
+
+// module.exports = class Device extends Sequelize.Model {
+//   static init(sequelize) {
+//     return super.init({
+//       departmentId: {
+//         type: Sequelize.INTEGER,
+//         allowNull: true
+//       },
+//       name: {
+//         type: Sequelize.STRING(100),
+//         allowNull: false,
+//       },
+//       userid: {
+//         type: Sequelize.STRING(255),
+//         unique: true,
+//         allowNull: false,
+//       },
+//       password: {
+//         type: Sequelize.STRING(500),
+//         allowNull: false,
+//       },
+//       role: {
+//         type: Sequelize.ENUM('admin','팀장','팀원','대기'),
+//       },
+//       email: {
+//         type: Sequelize.STRING(255),
+//       },
+//       phone: {
+//         type: Sequelize.STRING(255),
+//       },
+//       img: {
+//         type: Sequelize.STRING,
+//         allowNull: true,
+//       },
+//       updatedPwDate: {
+//         type: Sequelize.DATE,
+//       },
+//     }, {
+//       sequelize,
+//       // tableName: 'tableName', // table명을 수동으로 생성 함
+//       // freezeTableName: true, // true: table명의 복수형 변환을 막음
+//       underscored: true, // true: underscored, false: camelCase
+//       timestamps: true, // createAt, updatedAt
+//       paranoid: true, // deletedAt
+//     });
+//   }
+
+//   static associate(db) {
+//     db.User.belongsTo(db.Department, { foreignKey: { name: 'departmentId', as: 'Department' } });
+//   }
+// };
---
+++ routes/device.js
@@ -0,0 +1,6 @@
+// const express = require('express');
+// const router = express.Router();
+// const logger = require('../lib/logger');
+// const deviceService = require('../service/deviceService');
+
+// module.exports = router;
